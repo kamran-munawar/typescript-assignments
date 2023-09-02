7cb0aa24edb292da7cf60590ad3b56ff3a6ab99c
kamran-munawar <kamijee450@gmail.com>
2023-09-02 11:00:45 +0500

    Create 3.ts

---
+++ 3.ts
@@ -0,0 +1,8 @@
+let FirstName = "Kamran";
+let LastName= "Munawar";
+console.log(`Upper Case: ${FirstName.toUpperCase()} ${LastName.toUpperCase()}`);
+console.log(`Lower Case: ${FirstName.toLowerCase()} ${LastName.toLowerCase()}`);
+let titlecase1=(`Title Case: ${FirstName.charAt(0).toUpperCase()}${FirstName.slice(1)} ${LastName.charAt(0).toUpperCase()}${LastName.slice(1)}`)
+
+console.log(titlecase1)
+
