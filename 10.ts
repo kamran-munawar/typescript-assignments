f836e70ecc07a9b7710e4dd6067b96d04d7e72b0
kamran-munawar <kamijee450@gmail.com>
2023-09-02 11:01:34 +0500

    Create 10.ts

---
+++ 10.ts
@@ -0,0 +1,10 @@
+//Assignment No 9: What is My Favorite No? By Kamran Munawar Dated:25/8/2023
+
+let favnum1=6;
+console.log(`My Favorite no is ${favnum1}`);
+
+//Assignment No 8: Calculation Printing By Kamran Munawar Dated:25/8/2023
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
